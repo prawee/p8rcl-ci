b9c0562bc133c0092ab7ce7f02166dd392bf5e9f
Prawee Wongsa <prawee@hotmail.com>
2025-03-27 06:31:39 +0700

    demo 0.0.3 with params

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css'
 import { Report } from '@p8w/p8rcl-ci';
 
+const columns = [
+  { key: 'id', name: "No" },
+  { key: 'item', name: 'Item'}
+];
+
+const data = [
+  { id: 0, name: 'A' },
+  { id: 1, name: 'B' }
+];
+
 function App() {
   return (
-    <Report />
+    <Report 
+      header={columns}
+      data={data}
+    />
   )
 }
 
