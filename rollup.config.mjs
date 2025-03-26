81b0f91528362bc0bfcc7fd44f372eb23e719642
Prawee Wongsa <prawee@hotmail.com>
2025-03-26 11:52:48 +0700

    fix yarn build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 
 export default [
     {
