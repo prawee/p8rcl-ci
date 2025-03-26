eacdddfaab2399b94464777f2da6b02daaa7e8df
Prawee Wongsa <prawee@hotmail.com>
2025-03-26 15:07:24 +0700

    using style with page

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { PageProps } from "./types";
+import { Container } from "./styled";
 
 const Page: React.FC<PageProps> = ({ title, children }) => {
   return (
-    <>
+    <Container>
       <h1>{title}</h1>
       {children}
-    </>
+    </Container>
   )
 }
 
